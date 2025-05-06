213ac25cb023aa95e939579584321d921545db75
Vitão <153944395+FOXgamer13013@users.noreply.github.com>
2025-05-06 16:01:03 -0300

    transporte
    passando informações atualizadas do meu pc para o github

---
+++ src/Entrega 1/Frontend/frontend_beta/myapp/Login.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myapp
+{
+    public partial class Login : Form
+    {
+        private bool senhaVisivel = false;
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        public Login()
+        {
+            InitializeComponent();
+
+            panel1.MouseDown += Panel1_MouseDown;
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Login_ResizeBegin(object sender, EventArgs e)
+        {
+        }
+
+        private void Login_ResizeEnd(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click_1(object sender, EventArgs e) { }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            String Email = textBox1.Text;
+            String Senha = textBox2.Text;
+
+            if (Email == "usuario@gmail.com" && Senha == "senha123")
+            {
+                new index().Show();
+                this.Hide();
+            }
+            else
+            {
+                if (Email != "usuario@gmail.com")
+                {
+                    MessageBox.Show("Usuário não encontrado!");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("A senha está incorreta!");
+                    textBox2.Clear();
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+
+        private void pictureBox3_Click(object sender, EventArgs e) { }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Função para mudar a visibilidade da senha
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (senhaVisivel)
+            {
+                textBox2.UseSystemPasswordChar = true;
+                senhaVisivel = false;
+            }
+            else
+            {
+                textBox2.UseSystemPasswordChar = false;
+                senhaVisivel = true;
+            }
+        }
+
+        private void pictureBox8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
