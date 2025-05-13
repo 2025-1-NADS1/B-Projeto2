2845a74c565c32d27b3fc494fa1fa08c83b7952a
antoniomiche <antonio.michel@edu.fecap.br>
2025-05-13 20:17:52 -0300

    Comit 1
    Arquivos base em caso de erro, usar para testes em um código novo

---
+++ src/Entrega 1/Frontend/front_end_base/frontend_beta/myapp/main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace myapp
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+
+            // Vincula os eventos de clique aos botões
+            comodos.Click += (s, e) => MostrarLabel(label1);
+            button2.Click += (s, e) => MostrarLabel(label2);
+            button3.Click += (s, e) => MostrarLabel(label3);
+        }
+
+        private void MostrarLabel(Label labelParaMostrar)
+        {
+            // Esconde todos os labels
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+
+            // Esconde todos os botões
+            comodos.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+
+            // Mostra o label vinculado
+            labelParaMostrar.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
