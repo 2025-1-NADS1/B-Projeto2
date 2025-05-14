6b60a39755283fa498b19d88bdf8d62364562ad2
Vitão <153944395+FOXgamer13013@users.noreply.github.com>
2025-05-13 22:49:58 -0300

    tela de trocar senha
    130525

---
+++ src/Entrega 1/Frontend/frontend_beta/myapp/index.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myapp
+{
+    public partial class index : Form
+    {
+        public index()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string endpoint = textBox1.Text.Trim();
+
+            // Se estiver vazio, vai pra página padrão
+            if (string.IsNullOrEmpty(endpoint))
+                endpoint = "/";
+
+            string url = $"http://localhost:9999{endpoint}";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string resposta = await client.GetStringAsync(url);
+                    textBox1.Text = resposta;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show("Erro na requisição: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro inesperado: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ src/Entrega 1/Frontend/frontend_beta/myapp/main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace myapp
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+
+            // Vincula os eventos de clique aos botões
+            comodos.Click += (s, e) => MostrarLabel(label1);
+            button2.Click += (s, e) => MostrarLabel(label2);
+            button3.Click += (s, e) => MostrarLabel(label3);
+        }
+
+        private void MostrarLabel(Label labelParaMostrar)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Esconde todos os labels
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+
+            // Esconde todos os botões
+            comodos.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comodos_Click(object sender, EventArgs e)
+        {
+            // Esconde todos os labels
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+
+            // Esconde todos os botões
+            comodos.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+
+        }
+    }
+}
